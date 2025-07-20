8c1dc515045ad7cc473b3abccf7013627473e5da
xiaoze-xiaoze <hanze06@163.com>
2025-07-21 02:11:31 +0800

    Add files via upload

---
+++ Toy Text/Frozen Lake/Deep Q-learning/Config.py
@@ -0,0 +1,25 @@
+class Config:
+    # 网络参数
+    learning_rate = 0.001
+    gamma = 0.99
+    
+    # 经验回放参数
+    buffer_size = 20000
+    batch_size = 32
+    
+    # 目标网络同步频率
+    target_sync = 100
+    
+    # 训练参数
+    train_episodes = 500
+    
+    # 测试参数
+    test_episodes = 10
+    
+    # 环境参数
+    env_name = 'FrozenLake-v1'
+    max_episode_steps = 100
+    is_slippery = True
+
+    # 模型保存路径
+    model_filename = 'model/best_model.pth'
